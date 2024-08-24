34c837a606af87969f40001e4772b276771e0aeb
deepak github <dakshdeepak90@gmail.com>
2024-08-24 17:03:54 +0530

    first commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -10,7 +10,7 @@ const Navbar = () => {
 <nav className='flex flex-col lg:flex-row justify-between mx-6 py-3 mb-10 '>
         <div>
 <h3 className='text-xl font-bold text-gray-600'>{ new Date().toUTCString().slice(0,16)}</h3>
-<h1 className='text-2xl font-bold '>Flavoro Foods</h1>
+<h1 className='text-2xl font-bold '>Food Only</h1>
         </div>
         <div>
             <input type='search' name='search'
